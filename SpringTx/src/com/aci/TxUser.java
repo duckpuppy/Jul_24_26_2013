0302a8850fb13c646891adebae65d305f0c52603
Prabhu Sunderaraman <iamprabhu@yahoo.com>
2013-07-25 14:31:08 -0400

    Txn

--- SpringTx/src/com/aci/TxUser.java
+++ SpringTx/src/com/aci/TxUser.java
@@ -4,10 +4,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class TxUser {
-	public static void main(String[] args) {
+	public static void main(String[] args)throws Exception {
 		ApplicationContext context = new FileSystemXmlApplicationContext("bin/com/aci/beans.xml");
 		PersonDao personDao = (PersonDao)context.getBean("personDao");
-		personDao.addTwoPersons("Sam", 12, "John", 56);
+		System.out.println(personDao.getClass());
+		System.out.println(personDao.getClass().getSuperclass());
+		personDao.addTwoDogs("Snoopy", "Rover");
+		//personDao.addTwoPersons("Sam", 12, "Johnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn", 56);
 		System.out.println("Done");
 	}
 }
