2ae348fd2d94b4acca432e23879d8efcb6626793
Prabhu Sunderaraman <iamprabhu@yahoo.com>
2013-07-26 12:03:39 -0400

    MVC

---
+++ Day02Web/src/com/aci/Address.java
@@ -0,0 +1,25 @@
+package com.aci;
+
+public class Address {
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	private String street = "--ENTER--";
+	private String city;
+	private String state;
+}
---
+++ Day02Web/src/com/aci/Country.java
@@ -0,0 +1,24 @@
+package com.aci;
+
+public class Country {
+	private String name;
+	private String capital;
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Country(String name, String capital) {
+		this.name = name;
+		this.capital = capital;
+	}
+	public Country() {
+	}
+}
